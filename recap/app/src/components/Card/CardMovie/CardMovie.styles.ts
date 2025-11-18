6e890e6b648fc12ed1c1b9ef4485efb320573318
Nathieli Leal <nathieli@txfuel.com.br>
2025-11-18 11:15:36 -0300

    m

--- recap/app/src/components/Card/CardMovie/CardMovie.styles.ts
+++ recap/app/src/components/Card/CardMovie/CardMovie.styles.ts
@@ -2,17 +2,20 @@ import { StyleSheet } from "react-native";
 
 export const stylesheet = (theme: any) => StyleSheet.create({
     cardMovie: {
-        width: 100,
-        height: 145,
+        width: 105,
+        height: 150,
+        borderWidth: 2,
         borderRadius: 15,
         marginBottom: 10,
         marginHorizontal: 5,
+        alignItems: "center",
+        justifyContent: "center",
+        borderColor: theme.terciary
     },
     cardImage: {
-        width: 100,
-        height: 145,
-        borderRadius: 15,
-        marginBottom: 10,
+        width: '100%',
+        height: '100%',
+        borderRadius: 12,
         backgroundColor: theme.darkGrey,
     },
 });
