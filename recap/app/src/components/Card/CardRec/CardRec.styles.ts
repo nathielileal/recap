926969c3330e48c9461b87460f975d62f3987c47
Nathieli Leal <nathieli@txfuel.com.br>
2025-12-03 11:23:08 -0300

    m

--- recap/app/src/components/Card/CardRec/CardRec.styles.ts
+++ recap/app/src/components/Card/CardRec/CardRec.styles.ts
@@ -4,32 +4,33 @@ export const stylesheet = (theme: any) => StyleSheet.create({
     card: {
         margin: 5,
         padding: 10,
-        borderRadius: 15,
-        borderColor: theme.terciary,
         borderWidth: 1,
+        borderRadius: 15,
         paddingBottom: 10,
+        borderColor: theme.terciary,
     },
     header: {
         flexDirection: "row",
         marginBottom: 5,
     },
     info: {
-        flexDirection: "column",
         flex: 1,
+        flexDirection: "column",
     },
     infoUser: {
         flexDirection: "row",
         alignItems: "center",
     },
     title: {
+        flex: 1,
         paddingLeft: 5,
         fontWeight: 'bold',
         fontSize: 12,
         color: theme.terciary,
     },
     time: {
-        paddingLeft: 10,
         fontSize: 8,
+        paddingLeft: 10,
         color: theme.terciary,
     },
 });
